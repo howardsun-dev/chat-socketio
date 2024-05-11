b6888f1aa70f0723c47be03e0c009bf98cb56004
Howard Sun <komugi@gmail.com>
2024-05-11 15:13:17 -0400

    created mongodb connector

---
+++ backend/db/connectToDb.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const connectToDb = async () => {
+  try {
+    if (!process.env.MONGODB_URI) {
+      throw new Error('MONGODB_URI environment variable is not defined');
+    }
+
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log('[mongodb] Connected to MongoDB');
+  } catch (error) {
+    console.log('[mongodb] Error connecting to MongoDB:', error);
+  }
+};
+
+export default connectToDb;
