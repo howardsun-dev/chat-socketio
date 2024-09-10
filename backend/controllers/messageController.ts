3bc44984a87ea9b48d79d61e684aa25f38f53c32
Howard Sun <howardsun@pm.me>
2024-09-10 16:39:25 -0400

    messageController

--- backend/controllers/messageController.ts
+++ backend/controllers/messageController.ts
@@ -1,1 +1,15 @@
-import Conversations
+import Conversations from '../models/conversationModel';
+import Message from '../models/messageModel';
+import { getReceiverSocketId } from '../socket/socket';
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { message } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user_id;
+
+        let conversation = await Conversations.findOne({
+            participants: { $all: [senderId, receiverId] },
+        })
+    }
+}
